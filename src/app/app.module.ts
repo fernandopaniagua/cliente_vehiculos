3b649e2e4bf6219b06c3ebbd82aa59ef23a32b5a
Fernando Paniagua <fernando.paniagua.fsg.2021@gmail.com>
2022-04-08 13:57:43 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ListadoVehiculosComponent } from './components/listado-vehiculos/listad
 import { VehiculoComponent } from './components/vehiculo/vehiculo.component';
 
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
